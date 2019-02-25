90b46b43d7100ceec5b1824293757814141d4b20
Beatrice Thalo <17069266+BeatriceThalo@users.noreply.github.com>
2019-02-25 15:25:26 -0500

    both z and front

--- src/android/CameraPreview.java
+++ src/android/CameraPreview.java
@@ -284,15 +284,15 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
         //display camera bellow the webview
         if(toBack){
 
-          webView.getView().setBackgroundColor(0x0000ff00); // TODO use clear
+          webView.getView().setBackgroundColor(0xff00ff00); // TODO use clear not green
           webViewParent = webView.getView().getParent();
           //((ViewGroup)webViewParent.getView()).setBackgroundColor(0xff000000);
-          // ((ViewGroup)webView.getView()).bringToFront(); // Doesn't work, camera still in front of webview
+          ((ViewGroup)webView.getView()).bringToFront(); // Doesn't work, camera still in front of webview
           // ((ViewGroup)webView.getView()).invalidate(); // second part
           // webView.getView().bringToFront();
           // webView.getView().invalidate();
-          // ViewCompat.setTranslationZ(webView.getView(), 1.0);
-          // ViewCompat.setTranslationZ(webView, 1.0);
+          ViewCompat.setTranslationZ(webView.getView(), 4.0);
+          // ViewCompat.setTranslationZ(webView, 4.0);
           // ViewCompat.setTranslationZ(containerView, 0.0);
 
         }else{
@@ -317,9 +317,9 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
   public void onCameraStarted() {
     Log.d(TAG, "Camera started");
 
-    if (fragment.toBack) {
-      ((ViewGroup)webView.getView()).bringToFront(); // Wait until after the camera has moved itself to the front onStarting
-    }
+    // if (fragment.toBack) {
+    //   ((ViewGroup)webView.getView()).bringToFront(); // Wait until after the camera has moved itself to the front onStarting
+    // }
 
     PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "Camera started");
     pluginResult.setKeepCallback(true);
